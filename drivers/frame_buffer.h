99381a8205793f3d0f18f0e6ef607210cf4a98f0
Kap Petrov <123942802+KapPetrovTheGuy@users.noreply.github.com>
2023-03-01 22:12:46 +0530

    Delete drivers directory

--- drivers/frame_buffer.h
+++
@@ -1,9 +0,0 @@
-
-#ifndef FRAME_BUFFER_H
-#define FRAME_BUFFER_H
-
-void fb_write_cell(unsigned int i, char c, unsigned char fg, unsigned char bg);
-void fb_move_cursor(unsigned short pos);
-int fb_write(char *buf, unsigned int len);
-
-#endif
