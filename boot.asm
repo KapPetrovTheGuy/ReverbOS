1f537aaa20661b6478d5ee5dd13975e1d3c5d28e
Kap Petrov <123942802+KapPetrovTheGuy@users.noreply.github.com>
2023-03-02 14:34:49 +0530

    Add Read From Disk, Add Makefile

--- boot.asm
+++ boot.asm
@@ -1,11 +1,41 @@
-ORG 0x7c00
+ORG 0
 BITS 16
+_start:
+    jmp short start
+    nop
 
-start:
-    mov si, message
+ times 33 db 0
+
+start: 
+    jmp 0x7c0:step2
+
+
+step2:
+    cli
+    mov ax, 0x7c0
+    mov ds, ax
+    mov es, ax
+    mov ax, 0x00
+    mov ss, ax
+    mov sp, 0x7c00
+    sti
+
+    mov ah, 2
+    mov al, 1
+    mov ch, 0
+    mov cl, 2
+    mov dh, 0
+    mov bx, buffer
+    int 0x13
+    jc error
+    mov si, buffer
     call print
     jmp $
 
+error:
+    mov si, error_message
+    call print
+    jmp $
 
 print:
     mov bx, 0
@@ -23,7 +53,9 @@ print_char:
     int 0x10
     ret
 
-message: db "Welcome To ReverbOS!", 0
+error_message: db "Failed to load sector", 0
 
 times 510-($-$$) db 0
 dw 0xAA55
+
+buffer: 
